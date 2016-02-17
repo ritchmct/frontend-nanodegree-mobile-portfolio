1ef8f28a453a236693f3347f08fdbcf27cad64a6
Ritchie <ritchie_diaper@yahoo.co.uk>
2016-02-17 16:18:49 +0000

    Add fonts and update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,60 @@
+var del = require('del');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
 var gulp = require('gulp');
-var webserver = require('gulp-webserver');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
-var livereload = require('gulp-livereload');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
 
-var src = 'src';
-var dist = 'dist';
-
-var paths = {
-	js: src + 'js/*.js',
-	css: src + 'css/*.css',
-	html: src + '**/*.html'
-};
-
-// Start a webserver @ localhost:8000
-gulp.task('server', function () {
-  return gulp.src(dist + '/')
-    .pipe(webserver());
+// Initiate local server
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        },
+    })
 });
 
-// Combine & Compress JS into one script.js
-gulp.task('combine-js', function () {
-  return gulp.src(paths.js)
-    .pipe(concat('script.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(dist + '/js'));
+// Cleaning
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
 });
 
 // Compress HTML
 gulp.task('minify', function () {
-  return gulp.src(paths.html)
+  return gulp.src('src/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(dist));
+    .pipe(gulp.dest('dist'));
 });
 
-// Watch files and reload browser
-gulp.task('watch', function () {
-  livereload.listen();
-  gulp.watch(paths.js, ['combine-js']);
-  gulp.watch(paths.scss, ['compile-sass']);
-  gulp.watch(paths.html, ['compress-html']);
-  gulp.watch(dist + '/**').on('change', livereload.changed);
+// Use "build" comments in html to minify css and js
+gulp.task('useref', function() {
+    return gulp.src('src/**/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', [
-  'server', 'combine-js',
-  'compile-sass', 'compress-html',
-  'watch' ]);
+// Copy fonts
+gulp.task('fonts', function() {
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+// Watch for changes and reload
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.html', browserSync.reload);
+    gulp.watch('src/css/**/*.css', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('default', function(callback) {
+  runSequence(['browserSync', 'watch'], callback)
+});
+
+gulp.task('build', function(callback) {
+  runSequence('clean:dist', ['useref', 'fonts'], callback)
+});
